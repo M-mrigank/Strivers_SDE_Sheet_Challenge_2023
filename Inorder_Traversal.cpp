2b1d280ae69eb3960738c767e0e02e2a117e1143
M-mrigank <92224747+M-mrigank@users.noreply.github.com>
2023-06-08 13:50:00 +0530

    Create Inorder_Traversal.cpp

---
+++ Inorder_Traversal.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
+void chk(TreeNode* root, vector<int>&v){
+    if(!root){
+        return;
+    }
+    chk(root->left, v);
+    v.push_back(root->data);
+    chk(root->right, v);
+}
+vector<int> getInOrderTraversal(TreeNode *root)
+{
+    // Write your code here.
+    vector<int>ans;
+    chk(root, ans);
+    return ans;
+}
