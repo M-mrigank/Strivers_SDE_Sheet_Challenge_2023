8618eb0f143c2813ea2f1efd91a27f0143c8da62
M-mrigank <92224747+M-mrigank@users.noreply.github.com>
2023-06-08 13:20:42 +0530

    Create Floyd_Warshall.cpp

---
+++ Floyd_Warshall.cpp
@@ -0,0 +1,18 @@
+int floydWarshall(int n, int m, int src, int dest, vector<vector<int>> &edges) {
+    // Write your code here.
+    vector<int>v(n, 1e9);
+    v[src-1]=0;
+    for(int i=0; i<n; i++){
+        for(auto it: edges){
+            if(v[it[1]-1]>v[it[0]-1]+it[2] && v[it[0]-1]!=1e9){
+                v[it[1]-1]=v[it[0]-1]+it[2];
+            }
+        }
+    }
+    for(auto it: edges){
+        if(v[it[0]-1]!=1e9 && v[it[1]-1]>v[it[0]-1]+it[2]){
+            return -1;
+        }
+    }
+    return v[dest-1];
+}
