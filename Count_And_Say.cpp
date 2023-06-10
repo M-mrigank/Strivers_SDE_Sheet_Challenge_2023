9db79dfd072b7976b443798d060828e5147c827f
M-mrigank <92224747+M-mrigank@users.noreply.github.com>
2023-06-10 11:30:35 +0530

    Create Count_And_Say.cpp

---
+++ Count_And_Say.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h> 
+void str(string& ans){
+	string x;
+	int cnt=1;
+	char a;
+	for(int i=0; i<ans.size()-1; i++){
+		a=ans[i];
+		if(ans[i]==ans[i+1]){
+			cnt++;
+			//cout<<cnt<<endl;
+		}
+		else{
+			//cout<<"here"<<endl;
+			x+=char(cnt+'0');
+			x+=ans[i];
+			//cout<<x<<endl;
+			cnt=1;
+		}
+	}
+	if(ans[ans.size()-2]!=ans[ans.size()-1]){x+='1'; x+=ans[ans.size()-1];}
+	else{
+		//cout<<"here"<<endl;
+		x+=char(cnt+'0');
+		x+=a;
+		// cout<<x<<endl;
+		cnt=1;
+	}
+	ans.clear();
+	//cout<<x<<" "<<"here"<<endl;
+	ans=x;
+}
+string writeAsYouSpeak(int n) 
+{
+	// Write your code here.
+	if(n==1){return "1";}
+        string ans="11";
+        n-=2;
+        while(n--){
+            str(ans);
+        }
+        return ans;	
+}
