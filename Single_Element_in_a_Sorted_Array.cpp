48416b118ef9b1e6aa354738c9c4af581946c357
M-mrigank <92224747+M-mrigank@users.noreply.github.com>
2023-06-16 19:34:43 +0530

    Update Single_Element_in_a_Sorted_Array.cpp

--- Single_Element_in_a_Sorted_Array.cpp
+++ Single_Element_in_a_Sorted_Array.cpp
@@ -1,9 +1,34 @@
+#include<bits/stdc++.h>
 int singleNonDuplicate(vector<int>& arr)
 {
 	// Write your code here
-	int ans=0;
-	for(int i=0; i<arr.size(); i++){
-		ans^=arr[i];
+	if(arr.size()==1){return arr.back();}
+	if(arr[0]!=arr[1]){
+		return arr[0];
 	}
-	return ans;
+	if(arr[arr.size()-1]!=arr[arr.size()-2]){
+		return arr[arr.size()-1];
+	}
+	int i=0, j=arr.size()-1;
+	if(j==0){return arr[0];}
+	while(i<j){
+		int mid=i+(j-i)/2;
+		if(mid%2){
+			if(arr[mid]==arr[mid-1]){
+				i=mid+1;
+			}
+			else{
+				j=mid-1;
+			}
+		}
+		else{
+			if(arr[mid]==arr[mid+1]){
+				i=mid+2;
+			}
+			else{
+				j=mid;
+			}
+		}
+	}
+	return arr[i];
 }
