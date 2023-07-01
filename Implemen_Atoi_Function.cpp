569e94670ef1e1589260072214943f5dc797619d
M-mrigank <92224747+M-mrigank@users.noreply.github.com>
2023-07-01 11:55:40 +0530

    Create Implemen_Atoi_Function.cpp

---
+++ Implemen_Atoi_Function.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+int atoi(string str) {
+    // Write your code here.
+    int n = str.length();
+    bool neg=false;
+    string  ans ="";
+    for(int i=0; i<n; i++){
+        if(str[i]=='-')neg=true;
+        if(str[i]<65 && str[i]!='-'){
+            ans = ans+str[i];
+        }
+    }
+    if(neg==true)return -stoi(ans);
+    return stoi(ans);
+}
